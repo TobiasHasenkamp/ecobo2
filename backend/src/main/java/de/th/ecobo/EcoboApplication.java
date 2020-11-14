028277003348d459f16eaee16eeec2ba4a5cde83
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-14 13:11:11 +0100

    Initial commit

---
+++ backend/src/main/java/de/th/ecobo/EcoboApplication.java
@@ -0,0 +1,13 @@
+package de.th.ecobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoboApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/th/ecobo/EcoboApplicationTests.java
@@ -0,0 +1,13 @@
+package de.th.ecobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
